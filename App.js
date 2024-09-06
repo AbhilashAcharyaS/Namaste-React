369aef3706e4f28d0d842d7675e94c311db2b65c
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-09-06 21:10:18 +0530

    ep3 react element, component, jsx

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", {}, "Hello from React");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+// const jsxheading= <h1>Hi from react Jsx</h1>;
 
+// React element
+const heading= <h1>heading using react element</h1>
 
-/* <div id="id1">
-        <div id="id2">
-            <h1>Heading1</h1>
-            <h2>Heading2</h2>
+// React functional component
+const HeadingComponent = ()=>(
+    <div id="container">
+        <h1 className='heading'>Heading using react functionl component</h1>
+    </div>
+);
+
+// React component composition
+const Heading2=()=>{ return <h2>heading2</h2>};
+const Heading3=()=>(
+    <div>
+        <Heading2/>
+        <h3>Heading3</h3>
+    </div>
+);
+
+//React element inside component
+
+const Heading4=()=>{
+    return (
+        <div>
+            <h1> Hi</h1>
+            {heading}
         </div>
-    </div> */
-const heading= React.createElement('div',{id:"id1"},
-    React.createElement('div',{id:"id2"}, 
-        [React.createElement('h1',{},'Heading1'), React.createElement('h2',{},'Heading2')]));
+    );
+}
+
 
 const root= ReactDOM.createRoot(document.querySelector('#root'));
-root.render(heading)
+root.render(<Heading4/>);
